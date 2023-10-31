0679bc8cccc9558a74669d2385ca18fd1151417e
Luiz Mendes <luiz.mendes@in8.com.br>
2023-10-31 07:17:13 -0300

    chore: add initial config

---
+++ src/domain/Warehousing.ts
@@ -0,0 +1,26 @@
+type Item = {
+
+}
+
+class Warehousing {
+    constructor(){}
+
+    createStructure(name: String): void {
+        const items = {}
+    }
+
+    addItem(item: Item, stockID: string): Item {
+        const items = {}
+        return items
+    }
+
+    getItem(): Item {
+        const items = {}
+        return items
+    }
+
+    changeItemQuantity(itemID: string, quantity: number){
+
+    }
+
+}
